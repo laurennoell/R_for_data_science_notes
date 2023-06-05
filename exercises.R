48b8ce61cce466e852f500d213bd053f606e38e9
Anmol Bhullar <anmol@Anmols-MacBook-Air.local>
2023-06-05 16:53:46 -0400

    more work on chapt 8

--- exercises.R
+++ exercises.R
@@ -30,4 +30,31 @@ flights |>
 table2 |>
     pivot_wider(names_from=type,values_from=count) |>
     mutate(rate=cases/population * 10000)
-
+# Chapter 8
+# Section 2
+# Question 1
+    read_delim("filename.txt", sep = "|")
+# Question 2
+    # quote, col_names, col_types, col_select, id, locale, n
+    # a, quoted_na, trim_ws, skip, n_max, guess_max, name_repair, 
+    # num_threads, progress, show_col_types, skip_empty_rows, lazy 
+# Question 3
+    # The filename and the function used to determine the length of every field.
+    # There are four such functions: fwf_empty(), fwf_widths(), fwf_positions(), fwf_cols()
+# Question 4
+    read_csv("filename.csv", quote = "'")
+# Question 5
+    # a) Mismatched number of columns. Given two categories "a" and "b" but 3 columns worth of values. 
+    # b) Same problem as a)
+    # c) Quote is unclosed. 
+    # d) Data mismatch. Row 1 is numbers, row 2 is characters. 
+    # e) Need to specify the delimiter is ";". It causes there to be one column with one data value
+# Question 6
+    # a)
+    annoying |> select(`1`) 
+    # b) 
+    ggplot(annoying, aes(x=`1`, y=`2`)) + geom_point()
+    # c) 
+    annoying |> mutate(`3` = `2`/`1`)
+    # d) 
+    annoying |> mutate(`3` = `2`/`1`) |> rename("one" = `1`, "two" = `2`, "three" = `3`)
