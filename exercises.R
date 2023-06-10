4b0088d8c43a03b116f0d882b906f01f474390ba
Anmol Bhullar <anmol@Anmols-MacBook-Air.local>
2023-06-10 18:10:18 -0400

    started chapt 11

--- exercises.R
+++ exercises.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 # Chapter 5 
     # Section 5.6 
         # Question 1a)
@@ -107,61 +109,130 @@
                     size = 4,
                     stroke = 2
                 )
-        # Section 4
-            # Question 1
-                # ggplot will make a new grid for every unique value in the continuous variable. 
-            # Question 2
-                # The empty graphs indicate where the combination of drive and cylinder do not exist. For
-                # example, there are no 5 cylinder all wheel drive vehicles so the plot is empty. The given
-                # piece of R-code makes this easier to see since the intersection x=4 and y=5 is empty. 
-            # Question 3
-                # Essentially we are using facet_grid with only variable (similar to facet_wrap). "drv ~ ."
-                # makes it drv is the x-variable plotted against nothing. ". ~ cyl" will plot cyl as a y-variable
-                # plotted against nothing (similar to facet_wrap)
-            # Question 4
-                # Even with the different colourings on class, it is still hard to ascertain general patterns
-                # about the behaviours of each singular type of class. A facet wrap helps solve this problem.
-            # Question 5
-                # nrow: Sets the number of rows to be displayed. #ncol - Similar.
-                # dir: displays horizontally or vertically
-                # drop: does not display empty tables. 
-                # as.table: ordered like a table iwth highest values at bottom-right.
-                # scales: whether to have x and y variable be kept the same across all tables or individually fit to each.
-            # Question 6
-                # It is easier to compare when all the graphs are stacked on top of each other.
-                # As long as it makes sense to keep x-variable be the same length across all graphs, it should be stacked
-                # vertically rather than horizontally 
-            # Question 7
-                ggplot(mpg) + 
-                    geom_point(aes(x = displ, y = hwy)) + 
-                    facet_wrap(~drv, dir = "h")
-                # The labels are now on top rather than on the side.
-        # Section 5
-            # Question 1
-                # The default geom associated with stat_summary() is pointrange.
-                ggplot(diamonds) + 
-                    stat_summary(
-                        aes(x = cut, y = depth),
-                        fun.min = min,
-                        fun.max = max,
-                        fun = median,
-                        geom = pointrange
-                    )
-            # Question 2
-                # geom_bar() makes the height of the bar proportional to the number of cases in each group, geom_col()
-                # makes the height of the bars represent the values in the data. 
-            # Question 3
-                # geom_point() - stat_identity(): No transformation of data, used to make simple scattlerplots.
-                # geom_histogram() - stat_bin(): stat_bin() "bins" the data and geom_histogram visualizes it.
-                # geom_bar() - stat_count(): stat_count() counts # of occurences and geom_bar visualizes it.
-                # geom_boxplot() - stat_boxplot()
-                # geom_density() - stat_density()
-                # geom_smooth() - stat_smooth()
-                # geom_line() - stat_summary()
-                # in general, the stat function transforms the data in some manner and geom visualizes the transformed data.
-            # Question 4
-                # functionally the same as geom_smooth() but we can change the results with a non-standard geom.
-            # Question 5
-                # The "group = 1" argument is used to specify that the proportions are calculated over all bars together,
-                # not separately for each bar.
-
+    # Section 4
+        # Question 1
+            # ggplot will make a new grid for every unique value in the continuous variable. 
+        # Question 2
+            # The empty graphs indicate where the combination of drive and cylinder do not exist. For
+            # example, there are no 5 cylinder all wheel drive vehicles so the plot is empty. The given
+            # piece of R-code makes this easier to see since the intersection x=4 and y=5 is empty. 
+        # Question 3
+            # Essentially we are using facet_grid with only variable (similar to facet_wrap). "drv ~ ."
+            # makes it drv is the x-variable plotted against nothing. ". ~ cyl" will plot cyl as a y-variable
+            # plotted against nothing (similar to facet_wrap)
+        # Question 4
+            # Even with the different colourings on class, it is still hard to ascertain general patterns
+            # about the behaviours of each singular type of class. A facet wrap helps solve this problem.
+        # Question 5
+            # nrow: Sets the number of rows to be displayed. #ncol - Similar.
+            # dir: displays horizontally or vertically
+            # drop: does not display empty tables. 
+            # as.table: ordered like a table iwth highest values at bottom-right.
+            # scales: whether to have x and y variable be kept the same across all tables or individually fit to each.
+        # Question 6
+            # It is easier to compare when all the graphs are stacked on top of each other.
+            # As long as it makes sense to keep x-variable be the same length across all graphs, it should be stacked
+            # vertically rather than horizontally 
+        # Question 7
+            ggplot(mpg) + 
+                geom_point(aes(x = displ, y = hwy)) + 
+                facet_wrap(~drv, dir = "h")
+            # The labels are now on top rather than on the side.
+    # Section 5
+        # Question 1
+            # The default geom associated with stat_summary() is pointrange.
+            ggplot(diamonds) + 
+                stat_summary(
+                    aes(x = cut, y = depth),
+                    fun.min = min,
+                    fun.max = max,
+                    fun = median,
+                    geom = pointrange
+                )
+        # Question 2
+            # geom_bar() makes the height of the bar proportional to the number of cases in each group, geom_col()
+            # makes the height of the bars represent the values in the data. 
+        # Question 3
+            # geom_point() - stat_identity(): No transformation of data, used to make simple scattlerplots.
+            # geom_histogram() - stat_bin(): stat_bin() "bins" the data and geom_histogram visualizes it.
+            # geom_bar() - stat_count(): stat_count() counts # of occurences and geom_bar visualizes it.
+            # geom_boxplot() - stat_boxplot()
+            # geom_density() - stat_density()
+            # geom_smooth() - stat_smooth()
+            # geom_line() - stat_summary()
+            # in general, the stat function transforms the data in some manner and geom visualizes the transformed data.
+        # Question 4
+            # functionally the same as geom_smooth() but we can change the results with a non-standard geom.
+        # Question 5
+            # The "group = 1" argument is used to specify that the proportions are calculated over all bars together,
+            # not separately for each bar.
+    # Section 6
+        # Question 1
+            # There are a lot of overlapping data points. This can be seen by executing:
+            ggplot(mpg, aes(x = cty, y = hwy)) + geom_count()
+            # To fix, it we can use geom_jitter() instead of geom_point()
+        # Question 2
+            # There is no difference, position = "identity" is the default value assigned to the argument.
+            # identity places each point at its associated x and y value without any adjustment.
+        # Question 3
+            # 0 <= x, y <= 1.
+            geom_jitter(height = x, width = y)
+        # Question 4
+            # geom_jitter is geom_count with the additional argument of position = "jitter". It is easier
+            # to control the jitter using geom_jitter(height = x, width = y) arguments however. 
+        # Question 5
+            # The default position adjustment for geom_boxplot is "dodge2". 
+            ggplot(mpg, aes(manufacturer, cty)) + geom_boxplot()
+    # Section 7
+        # Question 1
+            ggplot(mpg, aes(x = manufacturer, fill = manufacturer)) +
+              geom_bar() + 
+              coord_polar()
+        # Question 2
+            # coord_map is a projection of an approximately spherical object onto a flat 2d plane that in general
+            # does not always preserve straight line whereas coord_quickmap is a quick approximation of this projection
+            # that does preserve straight lines.
+        # Question 3
+            # geom_abline adds a reference line to the plot. 
+            # coord_fixed makes it easier to quickly the line's slope (since it is not stretched horizontally)
+            # In general, the more miles a car can travel in city (per gallon), the more it can travel on a highway.
+# Chapter 11
+    # Section 4
+        # Question 1
+            # Missing values in a histogram are ignored because it deals with continuous
+            # values and grouping them in bins. There is no consistent way to do this with NA.
+            # With bars, we can count NA values under a separate NA category. For example:
+            df <- data.frame(c("a", "b", "c", NA), c(21, 22, 23, 24))
+            # identity stat tells geom_bar to consider count column as the internal count it uses
+            ggplot(df, aes(grp, count, fill = grp)) + geom_bar(stat = "identity")
+        # Question 2
+            # na.rm = TRUE implies missing values are removed before computation. If there is
+            # a missing value, computation returns NA.
+        # Question 3
+            # facetting the cancelled variable:
+            nycflights13::flights |> 
+              mutate(
+                cancelled = is.na(dep_time),
+                sched_hour = sched_dep_time %/% 100,
+                sched_min = sched_dep_time %% 100,
+                sched_dep_time = sched_hour + (sched_min / 60)
+              ) |> 
+              ggplot(aes(x = sched_dep_time, color = cancelled)) + 
+              geom_freqpoly(binwidth = 1/4) + 
+              facet_wrap(~cancelled)
+    # Section 5
+        # Question 1
+            # This makes it easier to compare the two different lines even if
+            # there is a large amount of uncancelled flights.
+            df <- nycflights13::flights |> 
+              mutate(
+                cancelled = is.na(dep_time),
+                sched_hour = sched_dep_time %/% 100,
+                sched_min = sched_dep_time %% 100,
+                sched_dep_time = sched_hour + (sched_min / 60)
+              )
+            ggplot(df, aes(sched_dep_time, after_stat("density"))) + 
+              geom_freqpoly(aes(color = cancelled), binwidth = 1/4)
+        # Question 2
+            
+        
